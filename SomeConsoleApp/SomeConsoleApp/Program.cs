7945864d12d7836e41e198dd6634d9f5f23bd1f6
valgaudeau <valgaudeau@outlook.com>
2022-06-23 11:08:13 +0100

    Simulated fix to page counter which now increments by 2 instead of 1

--- SomeConsoleApp/SomeConsoleApp/Program.cs
+++ SomeConsoleApp/SomeConsoleApp/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Not hello world");
+            Console.WriteLine("simulate fix for increment counter action which now increments 2 instead of 1");
         }
     }
 }
